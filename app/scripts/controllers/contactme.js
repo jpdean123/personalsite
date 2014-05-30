9167b5cc8bf7de11cc2c52e01b2bab0d1b3cbb4c
jpdean123 <jackdean@Jacks-MacBook-Pro.local>
2014-05-29 22:56:28 -0400

    Initial Commit

---
+++ app/scripts/controllers/contactme.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jackApp')
+  .controller('ContactmeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/interests.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jackApp')
+  .controller('InterestsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
